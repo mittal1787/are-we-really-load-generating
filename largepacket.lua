b6f1e1466dd29738c90ccbe5532b7f048bac6fdb
yugm2 <yugm2@node0.yugm2-188850.cs523-uiuc-sp24-pg0.clemson.cloudlab.us>
2024-02-14 23:19:06 -0500

    Clean up code

--- largepacket.lua
+++ largepacket.lua
@@ -9,11 +9,16 @@ local function send_file()
     local method = "POST"
     local path = url .. "/"
     local body = "data=" .. readAll("90MB")
+    print(len(body))
     local headers = {}
     headers["Content-Type"] = "application/json"
     headers["Accept"] = "application/json"
     return wrk.format(method, path, headers, body)
   end
+
+request = function()
+    return send_file(url)
+end
 
 response = function(status, headers, body)
     print(body)
