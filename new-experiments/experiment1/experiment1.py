ea822a95baf95a30d867702828144a4df23896d3
yugm2 <yugm2@dcl-l440-03.ews.illinois.edu>
2024-09-03 19:21:48 -0500

    update experiment 1

--- new-experiments/experiment1/experiment1.py
+++ new-experiments/experiment1/experiment1.py
@@ -7,8 +7,7 @@ def install_server(server_machine_name:str, username):
     ssh_con = paramiko.SSHClient()
     ssh_con.load_system_host_keys()
     ssh_con.connect(server_machine_name, username=username)
-    stdin, stdout, stderr = ssh_con.exec_command("git clone https://github.com/mittal1787/are-we-really-load-generating.git && cd are-we-really-load-generating && git pull origin main")
-    stdin, stdout, stderr = ssh_con.exec_command("cd are-we-really-load-generating/new-experiments/experiment1 && sh install.sh")
+    stdin, stdout, stderr = ssh_con.exec_command("git clone https://github.com/mittal1787/are-we-really-load-generating.git && cd are-we-really-load-generating && git pull origin main && cd new-experiments/experiment1 && sh install.sh")
     for i in range(4):
         try:
             stdin.write("Y\n")
