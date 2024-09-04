61bb213957da3fe8b68cd57b93a265a40f970812
Mittal <yugm2@illinois.edu>
2024-09-04 18:33:56 -0500

    Update experiment runs

--- new-experiments/experiment2/run_experiment2.sh
+++ new-experiments/experiment2/run_experiment2.sh
@@ -1,2 +1,2 @@
 pip3 install paramiko numpy matplotlib
-cd ../.. && python3 -m new-experiments.experiment2.experiment2 -c $1 -s $2 -u $3
+cd ../.. && python3 -m new-experiments.experiment2.experiment2 -c $0 -s $1 -u $2
