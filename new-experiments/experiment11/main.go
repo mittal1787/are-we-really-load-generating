b9ac35d97e3bf24b3c8c19ebebfa13d1849545f2
yugm2 <yugm2@node0.yugm2-215757.cs523-uiuc-sp24-pg0.utah.cloudlab.us>
2024-09-07 20:21:29 -0600

    add logger code

--- new-experiments/experiment11/main.go
+++ new-experiments/experiment11/main.go
@@ -10,6 +10,26 @@ import (
 	"os"
 )
 
+var file, _ = os.OpenFile("machine.i.log", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+
+func LogEvent(event string, rest ...any) {
+	w := bufio.NewWriter(file)
+
+	_, err := fmt.Fprintln(w, "[", time.Now().Unix(),"]",event, rest)
+	if err != nil {
+		panic(err)
+	}
+
+	// TODO: Use more efficient method later on writing to file immediately after logging
+	w.Flush()
+	file.Close()
+	file, err = os.OpenFile("machine.i.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+
 func main() {
 	wwwDir := flag.String("www", "www", "directory with files to be served")
 	wwwPort := flag.Int("port", 8001, "http port")
@@ -31,10 +51,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(time.Now().UnixNano())
 	duration, err := time.ParseDuration(os.Args[1])
 	if err == nil {
-		println(os.Args[1])
+		LogEvent(os.Args[1])
 		time.Sleep(duration)
 	} else {
-		println(err.Error())
+		LogEvent(err.Error())
 	}
 	fmt.Fprint(w, "Hello !")
 }
