fc3cf08aed38b173e3a4ffc4d8bf8c15d3a869a7
yugm2 <yugm2@node0.yugm2-216645.cloudlab-ym-pg0.utah.cloudlab.us>
2024-09-16 12:20:51 -0600

    update wrk2-dsb installation

--- new-experiments/install_wrk2_dsb.sh
+++ new-experiments/install_wrk2_dsb.sh
@@ -1,6 +1,8 @@
-git clone https://github.com/delimitrou/DeathStarBench.git
+git clone https://github.com/mittal1787/DeathStarBench.git
 sudo apt-get update
+sudo apt install lua5.4 liblua5.4-dev
 sudo apt install libssl-dev
+sudo apt install zlib1g
 cd DeathStarBench/wrk2
 git submodule update --init --recursive
 git submodule update --recursive --remote
